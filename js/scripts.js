65053c66e2841f0cb4bbe52906c2f3a47c613bd0
keebz <keebz@keebz.com>
2014-10-08 22:35:24 -0700

    formation and menu design

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  $('#MainLogo').hide();
+  $('#musicLayout').hide();
+  $('#mediaLayout').hide();
+  $('#liveLayout').hide();
+
 
   $('#MainLogo').click(function() {
     location.reload();
@@ -6,15 +11,35 @@ $(document).ready(function(){
 
   $("#miniBar")
     .mouseenter(function() {
-      $(this).animate({"width": "10em"}, 100);})
+          $('#musicLayout').removeClass("col-md-11");
+          $('#mediaLayout').removeClass("col-md-11");
+          $('#liveLayout').removeClass("col-md-11");
+      $(this).animate({"width": "16em"}, 100, function(){
+          $('#keebzIcon').hide();
+          $('#MainLogo').show();
+          $('#musicLayout').addClass("col-md-9");
+          $('#mediaLayout').addClass("col-md-9");
+          $('#liveLayout').addClass("col-md-9");
+
+        });
+      })
     .mouseleave(function() {
-      $(this).animate({"width": "3em"}, 100);
-  });
+          $('#MainLogo').hide();
+          $('#keebzIcon').show();
+      $(this).animate({"width": "5em"}, 100, function(){
+          $('#musicLayout').removeClass("col-md-9");
+          $('#musicLayout').addClass("col-md-11");
+          $('#mediaLayout').removeClass("col-md-9");
+          $('#mediaLayout').addClass("col-md-11");
+          $('#liveLayout').removeClass("col-md-9");
+          $('#liveLayout').addClass("col-md-11");
+        });
+      });
 
 //music Icon Function
   $('#musicIcon')
     .mouseenter(function() {
-      $('#musicBar').css("font-size","20px");
+      $('#musicBar').css("font-size","40px");
       $('#musicBar').animate({"opacity": "1" }, 100);})
     .mouseleave(function() {
       $("#musicBar").animate({"opacity": "0"}, 100);
@@ -33,7 +58,7 @@ $(document).ready(function(){
 //media Icon Function
   $('#mediaIcon')
     .mouseenter(function() {
-      $('#mediaBar').css("font-size","20px");
+      $('#mediaBar').css("font-size","40px");
       $('#mediaBar').animate({"opacity": "1" }, 100);})
     .mouseleave(function() {
       $("#mediaBar").animate({"opacity": "0"}, 100);
@@ -52,7 +77,7 @@ $(document).ready(function(){
 //live Icon Function
   $('#liveIcon')
     .mouseenter(function() {
-      $('#liveBar').css("font-size","20px");
+      $('#liveBar').css("font-size","40px");
       $('#liveBar').animate({"opacity": "1" }, 100);})
     .mouseleave(function() {
       $("#liveBar").animate({"opacity": "0"}, 100);
